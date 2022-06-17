a7e372d674fa8eb7ec6ee92c779cf78d0a498531
Đề Toàn Lê Trọng <letrongdetoan@gmail.com>
2022-06-17 09:17:07 +0700

    tim hieu ve export import

---
+++ Buoi-1/app.js
@@ -0,0 +1,4 @@
+import data, { x, y, z } from './index.js';
+
+console.log(x);
+console.log(data)
---
+++ Buoi-1/index.js
@@ -0,0 +1,7 @@
+let data = { name: "Toan", age: 30 }
+
+export let x = 123456;
+export let y = 12223456;
+export let z = 12333456;
+
+export default data;
