7f448731261904455110dafc19104de8d40a72da
Đề Toàn Lê Trọng <letrongdetoan@gmail.com>
2022-06-17 11:16:04 +0700

    copy file

--- Buoi-1/index.js
+++ Buoi-1/index.js
@@ -1,15 +1,4 @@
 const fs = require('fs');
 
-fs.readFile('./1.pdf', 'utf-8', function (err, data) {
-    if (err) {
-        console.log(err)
-    } else {
-        fs.writeFile('./hello1.txt', data, 'base64url', function () {
-            console.log('Da xong')
-        })
-
-    }
-})
-
-const test2 = fs.readFileSync('./test2.txt', 'utf-8');
-fs.writeFileSync('viet2.txt', test2, 'utf-8');
+const data = fs.copyFileSync('./hello.txt', './hw2lo1011.txt')
+console.log(data)
